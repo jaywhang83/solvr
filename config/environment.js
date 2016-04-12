8f530e6642626d95e692e7bc8fb2b06cca6c28a6
Simon Temple <simonctemple@live.com>
2016-04-12 11:43:50 -0700

    working profile

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'solvr',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://profile-data.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
