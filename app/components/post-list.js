ec313e335ab9b801fdffa2b6314dd68c6e75d845
lorenzo ferrario <lore.ferrario@gmail.com>
2016-04-12 11:23:49 -0700

    Add filtered post list

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedList: Ember.computed.sort('posts', 'filter'),
+  filter: ['reward:desc'],
+  filteredList: Ember.computed('sortedList', function(){
+    return this.get('sortedList').filter(function(post, index){
+      return index < 5;
+    });
+  })
 });
